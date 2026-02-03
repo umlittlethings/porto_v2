8c9f9792cb53ceff72851705912a335c116394f9
Chris Pradayana <sushitrash@Chriss-MacBook-Pro.local>
2026-02-03 14:01:39 +0700

    repair

--- src/components/Home/about/About.jsx
+++ src/components/Home/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <img className='scale-50 sm:scale-100 pt-5 sm:pt-20 px-12 sm:px-30' src="/about/about tulisan.svg" alt="about" />
       <div className='flex flex-col sm:flex-row gap-10 sm:gap-0'>
         <p className='font-Jakarta-Medium px-7 sm:px-30 pt-2 sm:pt-10 text-base sm:text-xl font-medium flex-1 text-justify'>
-          Hi, I'm Chris, a third-year Computer Science student at Universitas Brawijaya. I love building thoughtful digital products for web and mobile, where clean design meets solid code. Blending creativity, logic, and a passion for learning, I aim to turn ideas into tech that people enjoy using.
+          Hi, I’m Chris, a fresh graduate in Computer Science from Universitas Brawijaya. I love building thoughtful digital products for web and mobile, where clean design meets solid code. Blending creativity, logic, and a passion for learning, I aim to turn ideas into tech that people enjoy using.
         </p>
         <AnimatedLogoRow delayStart={0}>
           <div className='flex gap-0 sm:gap-0'>
